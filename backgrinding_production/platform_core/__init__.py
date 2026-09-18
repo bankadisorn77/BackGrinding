401e16a9ba2fd1e7ae0ff45c6562f8fbb94251f6
Adisorn <121882001+bankadisorn77@users.noreply.github.com>
2026-09-18 16:25:13 +0700

    Add platform core package

---
+++ backgrinding_production/platform_core/__init__.py
@@ -0,0 +1,6 @@
+from .context import InspectionContext
+from .camera_manager import CameraManager
+from .pipeline import PipelineEngine
+from .project import ProjectAdapter
+
+__all__ = ["InspectionContext", "CameraManager", "PipelineEngine", "ProjectAdapter"]
